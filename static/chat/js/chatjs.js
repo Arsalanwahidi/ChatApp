d229ea35f89f4ba2dcc405e836f7c77e48a2a52e
Arsalan Wahidi <arsalanwahidii@gmail.com>
2022-02-21 23:15:59 +0430

    New Changes

--- static/chat/js/chatjs.js
+++ static/chat/js/chatjs.js
@@ -2,36 +2,55 @@
 //Ajax implamention post and get request
 
 $(document).ready(function () {
-    var counter = 0;
+    var counter = 1;
     var idGroup = $('#groupName').text();
     $(document).on('submit', '#form_data', function (e) {
         e.preventDefault();
         
-        $.ajax({
-            type: 'POST',
-            url: `/postdata/${idGroup}`,
-            data: {
-                messages: $('#id_messages').val(),
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
-            },
-            success: function (response) {
-                $('#id_group').val('');
-            },
-            error: function (err) {
-                console.log(err)
-            },
-        });
+        if($('#id_messages').val() != ""){
+            $.ajax({
+                type: 'POST',
+                url: `/postdata/${idGroup}`,
+                data: {
+                    messages: $('#id_messages').val(),
+                    csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+                },
+                success: function (response) {
+                    $('#id_group').val('');
+                },
+                error: function (err) {
+                    console.log(err)
+                },
+            });
+        }
       
-        $.ajax({
-            type: 'GET',
-            url: `/getdata/${counter}`,
-            success: function (response) {
-                counter++;
-                $('#getData').append("" + response.messages + '<br>');
-            },
-            error: function (err) {
-                console.log(err);
-            },
-        });
+        setTimeout(() => {
+            $.ajax({
+                type: 'GET',
+                url: `/getdata/${counter}`,
+                success: function (response) {
+                    counter++;
+                    $('#getData').append("" + response.messages + '<br>');
+                },
+                error: function (err) {
+                    console.log(err);
+                },
+            });
+        }, 1400);
     });
-})
+
+    // setInterval(() => {
+    //     $.ajax({
+    //         type: 'GET',
+    //         url: `/getdata/${counter}`,
+    //         success: function (response) {
+    //             counter++;
+    //             $('#getData').append("" + response.messages + '<br>');
+    //         },
+    //         error: function (err) {
+    //             console.log(err);
+    //         },
+    //     });
+    // }, 4500);
+
+});
